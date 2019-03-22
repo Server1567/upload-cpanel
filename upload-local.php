bb4d83e8062e2bd9dbae0de467a7da4a8651c90a
Server1567 <chapijala@outlook.com>
2019-03-22 18:32:17 -0400

    jweofwehfiqwf

--- upload-local.php
+++ upload-local.php
@@ -87,6 +87,7 @@ if (!empty($_FILES)) {
         
         if (move_uploaded_file($source_path, $target_path)) {
             echo $id; // Manda el ID al FrontEnd(JS)
+            echo $source_path;
         }
 
     }
